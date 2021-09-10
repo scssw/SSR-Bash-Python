20533d6dbffef9670f3816e181f429e2269b32f3
scssw <90193529+scssw@users.noreply.github.com>
2021-09-10 20:27:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -209,7 +209,7 @@ if [[ -d /usr/local/SSR-Bash-Python ]];then
     if [[ $yn == [yY] ]];then
         rm -rf /usr/local/SSR-Bash-Python
         cd /usr/local
-        git clone https://git.fdos.me/stack/AR-B-P-B.git
+        git clone https://github.com/scssw/SSR-Bash-Python.git
         mv AR-B-P-B SSR-Bash-Python
     fi
     cd /usr/local/SSR-Bash-Python
@@ -221,7 +221,7 @@ if [[ -d /usr/local/SSR-Bash-Python ]];then
     fi
 else
     cd /usr/local
-    git clone https://git.fdos.me/stack/AR-B-P-B.git
+    git clone https://github.com/scssw/SSR-Bash-Python.git
     cd AR-B-P-B
     git checkout master
     if [[ $1 == "develop" ]];then
@@ -307,10 +307,10 @@ fi
 fi
 #Install SSR-Bash Background
 if [[ $1 == "develop" ]];then
-	wget -q -N --no-check-certificate -O /usr/local/bin/ssr https://git.fdos.me/stack/AR-B-P-B/raw/master/ssr
+	wget -q -N --no-check-certificate -O /usr/local/bin/ssr https://raw.githubusercontent.com/scssw/SSR-Bash-Python/master/ssr
 	chmod +x /usr/local/bin/ssr
 else
-	wget -q -N --no-check-certificate -O /usr/local/bin/ssr https://git.fdos.me/stack/AR-B-P-B/raw/master/ssr
+	wget -q -N --no-check-certificate -O /usr/local/bin/ssr https://raw.githubusercontent.com/scssw/SSR-Bash-Python/master/ssr
 	chmod +x /usr/local/bin/ssr
 fi
 
