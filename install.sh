0d4e55eb178f096f4bde0cd9d46d144d524549b1
scssw <90193529+scssw@users.noreply.github.com>
2024-12-29 20:30:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -396,5 +396,5 @@ fi
 
 # 添加新的任务
 (crontab -l ; echo "*/6 * * * * /bin/bash /usr/local/SSR-Bash-Python/timelimit.sh c > /dev/null 2>&1") | crontab -
-echo "已添加或更新为每30分钟执行的定时任务"
+echo "已添加或更新为每6分钟执行的定时任务"
 
