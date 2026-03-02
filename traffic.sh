e3f78153abf2e453b7c0ec8b5f06c3d28a1874a8
scssw <90193529+scssw@users.noreply.github.com>
2026-03-02 10:53:41 +0800

    Add files via upload

--- traffic.sh
+++ traffic.sh
@@ -6,6 +6,7 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 echo "1.显示所有用户流量信息"
 echo "2.清空指定用户流量"
+echo "3.一键修复到期时间"
 echo "直接回车返回上级菜单"
 
 while :; do echo
@@ -19,7 +20,14 @@ while :; do echo
 done
 
 if [[ $tc == 1 ]];then
-	python3 /usr/local/SSR-Bash-Python/show_flow.py
+	P_V=`python -V 2>&1 | awk '{print $2}'`
+	P_V1=`python -V 2>&1 | awk '{print $2}' | awk -F '.' '{print $1}'`
+	if [[ ${P_V1} == 3 ]];then
+		echo "你当前的python版本不支持此功能"
+		echo "当前版本：${P_V} ,请降级至2.x版本"
+	else
+		python /usr/local/SSR-Bash-Python/show_flow.py
+	fi
 	echo ""
 	bash /usr/local/SSR-Bash-Python/traffic.sh
 fi
@@ -40,16 +48,22 @@ if [[ $tc == 2 ]];then
 	if [[ $lsid == 1 ]];then
 		read -p "输入用户名： " uid
 		cd /usr/local/shadowsocksr
-		python3 mujson_mgr.py -c -u $uid
+		python mujson_mgr.py -c -u $uid
 		echo "已清空用户名为 ${uid} 的用户流量"
 	fi
 	
 	if [[ $lsid == 2 ]];then
 		read -p "输入端口号： " uid
 		cd /usr/local/shadowsocksr
-		python3 mujson_mgr.py -c -p $uid
+		python mujson_mgr.py -c -p $uid
 		echo "已清空端口号为${uid} 的用户流量"
 	fi
+	echo ""
+	bash /usr/local/SSR-Bash-Python/traffic.sh
+fi
+
+if [[ $tc == 3 ]];then
+	bash /usr/local/SSR-Bash-Python/repair_timelimit.sh
 	echo ""
 	bash /usr/local/SSR-Bash-Python/traffic.sh
 fi
