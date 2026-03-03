c6be0bd02e2207e320bb5d37934e54f3ebd8e7c8
scssw <90193529+scssw@users.noreply.github.com>
2026-03-03 14:21:02 +0800

    Add files via upload

--- self.sh
+++ self.sh
@@ -194,8 +194,8 @@ if [[ "$sv" == "$solve" ]];then
 		if [[ -e ./check.log ]];then
 			mv ./check.log /usr/local/SSR-Bash-Python/check.log
 		fi
-		if [[ -e ./timelimit.db ]];then
-			mv -f ./timelimit.db /usr/local/SSR-Bash-Python/timelimit.db
+		if [[ -e /usr/local/SSR-Bash-Python/timelimit.db ]];then
+			mv ./timelimit.db /usr/local/SSR-Bash-Python/timelimit.db
 		fi
 		if [[ ${OS} =~ ^Ubuntu$|^Debian$ ]];then
 			iptables-restore < /etc/iptables.up.rules
