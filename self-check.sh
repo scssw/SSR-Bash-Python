5f814d5230057bf57440771cfbdcc0481f8fbfe9
scssw <90193529+scssw@users.noreply.github.com>
2026-03-03 09:05:49 +0800

    Add files via upload

--- self-check.sh
+++ self-check.sh
@@ -5,39 +5,40 @@ echo "##################################
       AR-B-P-B 自检系统
              V1.0 Alpha
 ##################################"
-rm -f report.json
+report_path="/usr/local/SSR-Bash-Python/report.json"
+rm -f "${report_path}"
 #List /usr/local
-echo "############Filelist of /usr/local" >> report.json
+echo "############Filelist of /usr/local" >> "${report_path}"
 cd /usr/local
-ls >> report.json
+ls >> "${report_path}"
 #List /usr/local/ssr-bash-python3
-echo "############Filelist of /usr/local/SSR-Bash-Python" >> report.json
+echo "############Filelist of /usr/local/SSR-Bash-Python" >> "${report_path}"
 cd /usr/local/SSR-Bash-Python
-ls >> report.json
+ls >> "${report_path}"
 #List /usr/local/shadowsockr
-echo "############Filelist of /usr/local/shadowsockr" >> report.json
+echo "############Filelist of /usr/local/shadowsockr" >> "${report_path}"
 cd /usr/local/shadowsocksr
-ls >> report.json
-echo "############File test" >> report.json
+ls >> "${report_path}"
+echo "############File test" >> "${report_path}"
 #Check File Exist
 if [ ! -f "/usr/local/bin/ssr" ]; then
   echo "SSR-Bash-Python主文件缺失，请确认服务器是否成功连接至Github"
-  echo "SSR Miss" >> report.json
+  echo "SSR Miss" >> "${report_path}"
   exit
 fi
 if [ ! -f "/usr/local/SSR-Bash-Python/server.sh" ]; then
   echo "SSR-Bash-Python主文件缺失，请确认服务器是否成功连接至Github"
-  echo "SSR Miss" >> report.json
+  echo "SSR Miss" >> "${report_path}"
   exit
 fi
 if [ ! -f "/usr/local/shadowsocksr/stop.sh" ]; then
   echo "SSR主文件缺失，请确认服务器是否成功连接至Github"
-  echo "SSR Miss" >> report.json
+  echo "SSR Miss" >> "${report_path}"
   exit
 fi
 
 #Firewall
-echo "############Firewall test" >> report.json
-iptables -L >> report.json
+echo "############Firewall test" >> "${report_path}"
+iptables -L >> "${report_path}"
 
 echo "检测完成，未发现严重问题，如仍有任何问题请提交report.json"
